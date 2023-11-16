b7051e44cb15aa7dd4946ae3d3dee781917256be
wbdvlpmnt <jai.gokhale5219@gmail.com>
2023-11-16 08:32:15 -0600

    scaffolding html

--- src/App.js
+++ src/App.js
@@ -1,3 +1,25 @@
+// declare outside, else recreated on every render
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 export default function App() {
-  return <div>Hello React</div>;
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className="active">1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+
+      <p className="message">Hello</p>
+
+      <div className="buttons">
+        <button style={{ backgroundColor: "#7950f2" }}>Previous</button>
+        <button style={{ backgroundColor: "#7950f2" }}>Next</button>
+      </div>
+    </div>
+  );
 }
