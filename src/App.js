d1161b7c7f3b86d5d19196bebe3b89c9963fba74
wbdvlpmnt <jai.gokhale5219@gmail.com>
2023-11-29 15:48:58 -0600

    using React children prop

--- src/App.js
+++ src/App.js
@@ -41,18 +41,32 @@ export default function App() {
           </p>
 
           <div className="buttons">
-            <button
-              style={{ backgroundColor: "#7950f2" }}
+            <Button
+              bgColor={"#7950f2"}
+              textColor={"#fff"}
               onClick={handlePrevious}
             >
-              Previous
-            </button>
-            <button style={{ backgroundColor: "#7950f2" }} onClick={handleNext}>
-              Next
-            </button>
+              <span>👈</span>Previous
+            </Button>
+
+            <Button bgColor={"#7950f2"} textColor={"#fff"} onClick={handleNext}>
+              {" "}
+              Next<span>👉</span>
+            </Button>
           </div>
         </div>
       )}
     </>
+  );
+}
+
+function Button({ textColor, bgColor, onClick, children }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
   );
 }
